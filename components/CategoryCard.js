019ed2f60bc6726627ef0bda457d5a120171c25f
rbpolim <rodrigobpolim@gmail.com>
2022-09-30 15:19:30 -0300

    feat: created & styled CategoryCard component

---
+++ components/CategoryCard.js
@@ -0,0 +1,16 @@
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import React from "react";
+
+const CategoryCard = ({ name, image }) => {
+  return (
+    <TouchableOpacity className="relative mr-2">
+      <Image source={{ uri: image }} className="w-20 h-20 rounded" />
+
+      <Text className="absolute font-bold text-white bottom-1 left-1">
+        {name}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default CategoryCard;
